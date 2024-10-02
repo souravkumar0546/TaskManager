2ccfdcbcbe10df875b4f305e49906fb79661ed95
Sourav Kumar <souravkumar0546@gmail.com>
2024-10-02 07:08:05 +0530

    updated utils.go

--- backend/controllers/utils.go
+++ backend/controllers/utils.go
@@ -34,9 +34,32 @@ func writeResponse(w http.ResponseWriter, response interface{}, err error) {
 }
 
 // writeErrorResponse creates an error response and writes to the user.
+// func writeErrorResponse(w http.ResponseWriter, err error) {
+// 	w.Header().Set("Content-Type", "application/json")
+// 	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("APP_URL"))
+// 	statusCode := internal_errors.GetStatusFromError(err)
+// 	w.WriteHeader(statusCode)
+// 	type genericError struct {
+// 		Message string `json:"message"`
+// 	}
+// 	errResponse := genericError{
+// 		Message: err.Error(),
+// 	}
+// 	if writeErr := json.NewEncoder(w).Encode(errResponse); writeErr != nil {
+// 		zap.NewExample().Error("could not write response", zap.Error(writeErr))
+// 	}
+// }
 func writeErrorResponse(w http.ResponseWriter, err error) {
 	w.Header().Set("Content-Type", "application/json")
-	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("APP_URL"))
+
+	// Set allowed origins based on your environment
+	appURL := os.Getenv("APP_URL")
+	localhost := "http://localhost:3000"
+	
+	// You can dynamically decide if you're in development or production
+	allowedOrigins := fmt.Sprintf("%s, %s", appURL, localhost)
+	w.Header().Set("Access-Control-Allow-Origin", allowedOrigins)
+
 	statusCode := internal_errors.GetStatusFromError(err)
 	w.WriteHeader(statusCode)
 	type genericError struct {
