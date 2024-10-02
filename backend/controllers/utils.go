68284ec68bba6c5383befce3c781112fb6767af5
Sourav Kumar <souravkumar0546@gmail.com>
2024-10-02 07:28:53 +0530

    updated utils.go

--- backend/controllers/utils.go
+++ backend/controllers/utils.go
@@ -21,45 +21,38 @@ func fetch_user_id(r *http.Request) (int, error) {
 	return userID.(int), nil
 }
 
-// writeResponse receives the response and error and appropriately fills the response status, header and body
-func writeResponse(w http.ResponseWriter, response interface{}, err error) {
+// writeResponse receives the response and error and appropriately fills the response status, header, and body
+func writeResponse(w http.ResponseWriter, response interface{}, err error, r *http.Request) { // Added r *http.Request here
 	if err != nil {
-		writeErrorResponse(w, err)
+		writeErrorResponse(w, r, err) // Pass the request here
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
-	if writeErr := json.NewEncoder(w).Encode(response); writeErr != nil {
-		zap.NewExample().Error("could not write response", zap.Error(writeErr))
+	
+	// Check if response is not nil before encoding
+	if response != nil {
+		if writeErr := json.NewEncoder(w).Encode(response); writeErr != nil {
+			zap.NewExample().Error("could not write response", zap.Error(writeErr))
+		}
 	}
 }
 
 // writeErrorResponse creates an error response and writes to the user.
-// func writeErrorResponse(w http.ResponseWriter, err error) {
-// 	w.Header().Set("Content-Type", "application/json")
-// 	w.Header().Set("Access-Control-Allow-Origin", os.Getenv("APP_URL"))
-// 	statusCode := internal_errors.GetStatusFromError(err)
-// 	w.WriteHeader(statusCode)
-// 	type genericError struct {
-// 		Message string `json:"message"`
-// 	}
-// 	errResponse := genericError{
-// 		Message: err.Error(),
-// 	}
-// 	if writeErr := json.NewEncoder(w).Encode(errResponse); writeErr != nil {
-// 		zap.NewExample().Error("could not write response", zap.Error(writeErr))
-// 	}
-// }
-func writeErrorResponse(w http.ResponseWriter, err error) {
+func writeErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	w.Header().Set("Content-Type", "application/json")
 
-	// Set allowed origins based on your environment
-	appURL := os.Getenv("APP_URL")
-	localhost := "http://localhost:3000"
+	// Get the origin of the request
+	origin := r.Header.Get("Origin")
 	
-	// You can dynamically decide if you're in development or production
-	allowedOrigins := fmt.Sprintf("%s, %s", appURL, localhost)
-	w.Header().Set("Access-Control-Allow-Origin", allowedOrigins)
+	// Allow specific origins
+	appURL := os.Getenv("APP_URL") // Netlify URL
+	localhost := "http://localhost:3000"
+
+	// Check if the origin is allowed
+	if origin == appURL || origin == localhost {
+		w.Header().Set("Access-Control-Allow-Origin", origin)
+	}
 
 	statusCode := internal_errors.GetStatusFromError(err)
 	w.WriteHeader(statusCode)
